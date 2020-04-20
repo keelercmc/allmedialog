ed2fb9feeca098d6c2a16eb8e1369c4eae7a01aa
Chance <keelercmc@gmail.com>
2020-04-20 03:19:05 -0400

    Link axios and firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+const test = () => {
+  const post = '5';
+  axios.post('https://allmedialog.firebaseio.com/example.json', post);
+}
 
 function App() {
   return (
     <div className="App">
-      amazing list
+      <button onClick={test}>hi</button>
     </div>
   );
 }
