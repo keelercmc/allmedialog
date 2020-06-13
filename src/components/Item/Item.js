cd8cbff3e0dedd305bf799b650bec9ff0a7782ca
Chance <keelercmc@gmail.com>
2020-06-13 11:56:26 -0400

    Add smartphone breakpoint

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -10,18 +10,21 @@ const item = (props) => {
     return (
         <div>
             <tr>
-                <td className={classes.Title}>{props.title}</td>
+                <td className={classes.Title}>{props.title}</td>                
                 <td className={classes.Creator}>{props.creator}</td>
                 <td className={classes.Score}>{props.score}</td>
                 <td className={classes.Year}>{props.year}</td>
                 <td className={classes.Type}>{props.type}</td>
                 
-                <td>
+
+                <td className={classes.ItemButtons}>
                     <Button variant='link' size='sm' onClick={() => props.toggleForm('hideUpdateForm', props.index)}>edit</Button>
                 </td>
-                <td>
+                <td className={classes.ItemButtons}>
                     <Button variant='link' size='sm' onClick={() => props.remove(props.extra)}>remove</Button>
                 </td>
+
+                
                 
             </tr>
                 {props.index === props.target ? <ListEntry {...props}></ListEntry>: null}
